ddb72accf6dc6a122294d88833f37b5ea5062330
Gantugs <gantugs01300130@gmail.com>
2018-12-21 11:19:02 +0800

    ?

--- lab11/main.cpp
+++ lab11/main.cpp
@@ -8,67 +8,7 @@ static float g = 0;
 Universe uni;
 double alpha =PI/90;
 static float normalx=0, normaly=1, normalz=0;
-static float posx=500.0f,posz=-1800.0f,posy=500.0f;
-void SpecialKeys(int key, int x0, int y0)
-    {
-    if(key == GLUT_KEY_UP){
-        posy = cos(PI/90) * posy - sin(PI/90) * posz;
-        posz = cos(PI/90) * posz + sin(PI/90) * posy;
-        normaly=cos(PI/90) * normaly - sin(PI/90) * normalz;
-        normalz=cos(PI/90) * normalz + sin(PI/90) * normaly;  
-        glutPostRedisplay();
-    }
-    else {if(key == GLUT_KEY_DOWN){
-        posy = cos(PI/90) * posy + sin(PI/90) * posz;
-        posz = cos(PI/90) * posz - sin(PI/90) * posy;
-        normaly=cos(PI/90) * normaly + sin(PI/90) * normalz;
-        normalz=cos(PI/90) * normalz - sin(PI/90) * normaly;  
-        glutPostRedisplay();
-    }
-        else{
-            if(key==GLUT_KEY_RIGHT){
-            posx = cos(PI/90) * posx - sin(PI/90) * posy;
-            posy = cos(PI/90) * posy + sin(PI/90) * posx;
-            normaly=cos(PI/90) * normaly + sin(PI/90) * normalx;
-            normalx=cos(PI/90) * normalx - sin(PI/90) * normaly; 
-            glutPostRedisplay();
-            }
-            else{ if(key == GLUT_KEY_LEFT){
-            posx = cos(PI/90) * posx + sin(PI/90) * posy;
-            posy = cos(PI/90) * posy - sin(PI/90) * posx;
-            normaly=cos(PI/90) * normaly - sin(PI/90) * normalx;
-            normalx=cos(PI/90) * normalx + sin(PI/90) * normaly; 
-            glutPostRedisplay();
-            }
-            else{
-                if(key == GLUT_KEY_PAGE_UP){
-                    float R = sqrt(posx*posx+posy*posy+posz*posz);
-                    float Rotz = posz/R;
-                    posz = (R - 5)*Rotz;
-                    float Roty = posy/R;
-                    posy = (R - 5)*Roty;
-                    float Rotx = posx/R;
-                    posx = (R - 5)*Rotx;
-                    glutPostRedisplay();
-                    }
-                    else{
-                        if(key == GLUT_KEY_PAGE_DOWN){
-                            float R = sqrt(posx*posx+posy*posy+posz*posz);
-                            float Rotz = posz/R;
-                            posz = (R + 5)*Rotz;
-                            float Roty = posy/R;
-                            posy = (R + 5)*Roty;
-                            float Rotx = posx/R;
-                            posx = (R + 5)*Rotx;
-                            glutPostRedisplay();
-                        }
-                    }
-            }
-            }
-        }
-    }
-
-    }
+static float posx=0.0f,posz=-1800.0f,posy=-500.0f;
 void resize(int w, int h)
 {
 	glViewport(0, 0, w, h);
@@ -76,7 +16,6 @@ void resize(int w, int h)
 
 	glMatrixMode(GL_PROJECTION);
     gluPerspective(35.0f, fAspect, 1.0, 10000.0);
-  //  glShadeModel(GL_SMOOTH);
 	glMatrixMode(GL_MODELVIEW);
 	glLoadIdentity();
 
@@ -91,11 +30,11 @@ void timer(int value) {
 void setup()
 {   
     uni.file_read("./nbodys.txt");
-	GLfloat  ambientLight[] = {0.4f, 0.4f, 0.4f, 1.0f };
-    GLfloat  diffuseLight[] = {0.7f, 0.7f, 0.7f, 1.0f };
+	GLfloat  ambientLight[] = {0.2f, 0.2f, 0.2f, 1.0f };
+    GLfloat  diffuseLight[] = {0.3, 0.3f, 0.3f, 1.0f };
     GLfloat  specular[] = { 0.9f, 0.9f, 0.9f, 1.0f};
-    GLfloat  lightPos[] = { 0.0f, 40.0f, 0.0f, 1.0f };
-    GLfloat  specref[] =  { 0.6f, 0.6f, 0.6f, 1.0f };
+    GLfloat  lightPos[] = { 500.0f, 500.0f, 30.0f, 1.0f };
+    GLfloat  specref[] =  { 0.9f, 0.9f, 0.9f, 1.0f };
         glEnable(GL_DEPTH_TEST);    // Hidden surface removal
         glEnable(GL_CULL_FACE);        // Do not calculate inside of solid object
         glFrontFace(GL_CCW);
@@ -124,7 +63,7 @@ void setup()
  //   with a moderate shine
     glMaterialfv(GL_FRONT, GL_SPECULAR,specref);
     glMateriali(GL_FRONT,GL_SHININESS,64);
-    glClearColor(1.0f, 1.0f, 1.0f, 0.0f);
+    glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
 }
 void draw_body(Body x, int i){
     glPushMatrix();
@@ -137,15 +76,15 @@ void draw_body(Body x, int i){
         break;
         case 1:
             glColor3f(150/s, 247/s, 139/s);
-            glutSolidSphere(5,50,50);
+            glutSolidSphere(10,50,50);
         break;
         case 2:
             glColor3f(30/s, 158/s, 158/s);
-            glutSolidSphere(6,50,50);
+            glutSolidSphere(10,50,50);
         break;
         case 3:
             glColor3f(30/s, 90/s, 158/s);
-            glutSolidSphere(7,50,50);
+            glutSolidSphere(10,50,50);
         break;
         case 4:
             glColor3f(158/s, 30/s, 128/s);
@@ -153,10 +92,10 @@ void draw_body(Body x, int i){
         break;
         case 5:
             glColor3f(222, 0, 0);
-            glutSolidSphere(6,50,50);
+            glutSolidSphere(8,50,50);
         break;
         case 6:
-            glColor3f(18/s, 20/s, 0);
+            glColor3f(18/s, 200/s, 0);
             glutSolidSphere(4,50,50);
         break;
     }
@@ -164,10 +103,9 @@ void draw_body(Body x, int i){
     glPopMatrix();
 }
 void myDisplay() {
- //   uni.update(0.01);
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 	glPushMatrix();
-    gluLookAt(	posx, posy, posz,
+    gluLookAt(	posx+500, posy+500, posz,
 			500, 500, 0,
 			normalx, normaly, normalz); 
 
@@ -186,9 +124,8 @@ int main(int argc, char *argv[]) {
     glutCreateWindow("Universe");   
     glutReshapeFunc(resize);
     glutDisplayFunc(myDisplay);
-    glutSpecialFunc(SpecialKeys);
     setup();
-    glutTimerFunc(1, timer, 1);
+    glutTimerFunc(33, timer, 1);
     glutMainLoop();
     return 0;
 }
